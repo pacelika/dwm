1f83cbbf0ec6f9ebdb7564e86caa40867b641663
pacelika <tamtammacro@gmail.com>
2025-01-25 12:45:39 -0800

    added colors.h

--- headers/appearance.h
+++ headers/appearance.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <def.h>
+#include "colors.h"
 
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
@@ -8,16 +9,31 @@ static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
 static const char *fonts[] = {"monospace:size=10"};
 static const char dmenufont[] = "monospace:size=10";
-static const char col_gray1[] = "#222222";
-static const char col_gray2[] = "#444444";
-static const char col_gray3[] = "#bbbbbb";
-static const char col_gray4[] = "#eeeeee";
-static const char col_cyan[] = "#005577";
+
+#define SCHEME_NAME_PINK
+
+#ifdef SCHEME_NAME_PINK
+static const char* const scheme_normal_fg = col_white;
+static const char* const scheme_normal_bg = col_black;
+static const char* const scheme_normal_border = col_black;
+
+static const char* const scheme_sel_fg = col_black;
+static const char* const scheme_sel_bg = col_pink;
+static const char* const scheme_sel_border = col_pink;
+#else
+static const char* const scheme_normal_fg = col_gray3;
+static const char* const scheme_normal_bg = col_gray1;
+static const char* const scheme_normal_border = col_gray2;
+
+static const char* const scheme_sel_fg = col_gray4;
+static const char* const scheme_sel_bg = col_cyan;
+static const char* const scheme_sel_border = col_cyan;
+#endif
 
 static const char *colors[2][3] = {
     /*               fg         bg         border   */
-    [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
-    [SchemeSel] = {col_gray4, col_cyan, col_cyan},
+    [SchemeNorm] = {scheme_normal_fg, scheme_normal_bg, scheme_normal_border},
+    [SchemeSel] = {scheme_sel_fg, scheme_sel_bg, scheme_sel_border},
 };
 
 static const char *tags[9] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
---
+++ headers/colors.h
@@ -0,0 +1,14 @@
+#ifndef DWM_COLORS_H
+#define DWM_COLORS_H
+
+static const char col_gray1[] = "#222222";
+static const char col_gray2[] = "#444444";
+static const char col_gray3[] = "#bbbbbb";
+static const char col_gray4[] = "#eeeeee";
+static const char col_cyan[] = "#005577";
+
+static const char col_black[] = "#000000";
+static const char col_white[] = "#ffffff";
+static const char col_pink[] = "#FFC0CB";
+
+#endif
